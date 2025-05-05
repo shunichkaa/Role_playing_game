541a03309e471884695b767de7db7633d79b42c7
Aleksandra Mate <matealeksandra@gmail.com>
2025-05-05 16:49:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
 }
 function goCave () {
   console.log("Going to cave.")
