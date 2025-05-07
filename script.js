747805d431bab648cd6e395db3cbb7d25d941274
Aleksandra Mate <matealeksandra@gmail.com>
2025-05-07 18:17:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,7 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [];
-const cat = {
-  name: "Whiskers",
-  "Number of legs": 4,
-}
-console.log(cat["Number of legs"]);
+
 
 // initialize buttons
 button1.onclick = goStore;
