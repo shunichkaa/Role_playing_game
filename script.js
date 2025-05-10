55fe2b08b9a8f950d61da23175922ca4e6dfa708
Aleksandra Mate <matealeksandra@gmail.com>
2025-05-10 17:29:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -69,12 +69,13 @@ function fightDragon() {
 }
 
 function buyHealth() {
-  gold -= 10;
-  health += 10;
-  goldText.innerText = gold;
-  healthText.innerText = health;
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  }
 }
-
 
 function buyWeapon() {
 
