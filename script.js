a4d271bc4f6817979e96cab97a3b525e1de14ae3
Aleksandra Mate <matealeksandra@gmail.com>
2025-05-11 21:08:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [
   {
-  name: "town square"
-}
+    name: "town square",
+    "button text": []
+  }
 ];
 
 // initialize buttons
