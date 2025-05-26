d6edb49377b26eefaf7a845fe08505089d7ebd6f
Aleksandra Mate <matealeksandra@gmail.com>
2025-05-26 18:10:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (currentWeaponIndex < weapons.length) {
+  if (currentWeaponIndex < weapons.length - 1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
@@ -98,9 +98,10 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
-
 
 function fightSlime() {
 
