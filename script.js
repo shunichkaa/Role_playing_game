cbcbbad858321d3fcaf0ec8736918dc276549f15
Aleksandra Mate <matealeksandra@gmail.com>
2025-05-28 11:58:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,10 @@ function sellWeapon() {
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
     text.innerText = "You sold a " + currentWeapon + ".";
-text.innerText += " In your inventory you have: " + inventory
+    text.innerText += " In your inventory you have: " + inventory;
+  }
+  else {
+    text.innerText = "Don't sell your only weapon!"
   }
 }
 
