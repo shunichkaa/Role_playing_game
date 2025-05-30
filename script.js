dce67e18f9f0d989572598472e3b842c445b2f0a
Aleksandra Mate <matealeksandra@gmail.com>
2025-05-30 12:51:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -107,12 +107,8 @@ function buyWeapon() {
 
 function sellWeapon() {
   if (inventory.length > 1) {
-    let soldWeapon = inventory.pop();
     gold += 15;
     goldText.innerText = gold;
-    text.innerText = "You sold your " + soldWeapon + ". In your inventory you have: " + inventory;
-  } else {
-    text.innerText = "Don't sell your only weapon!";
   }
 }
 
