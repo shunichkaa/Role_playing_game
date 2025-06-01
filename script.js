f1e355244d9d427fb9789786194f1021f5186d13
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-01 11:35:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,11 +22,6 @@ const weapons = [
   { name: 'claw hammer', power: 50 },
   { name: 'sword', power: 100 }
 ];
-const monsters = [
-  { name: "slime", level: 2, health: 15 },
-  { name: "fanged beast", level: 8, health: 60 },
-  { name: "dragon", level: 20, health: 300 }
-];
 const locations = [
   {
     name: "town square",
@@ -75,6 +70,10 @@ function goCave() {
   update(locations[2]);
 }
 
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
+
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -110,11 +109,7 @@ function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
-    let currentWeapon = inventory.shift();
-    text.innerText = "You sold a " + currentWeapon + ".";
-    text.innerText += " In your inventory you have: " + inventory;
-  } else {
-    text.innerText = "Don't sell your only weapon!";
+    let currentWeapon = inventory.shift()
   }
 }
 
@@ -124,8 +119,4 @@ function fightSlime() {
 
 function fightBeast() {
 
-}
-
-function fightDragon() {
-  console.log("Fighting dragon.");
 }
