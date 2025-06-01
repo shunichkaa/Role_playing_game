d198a90befd580700ab6959c9355af15d67a1ca0
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-01 11:36:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,9 @@ function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
-    let currentWeapon = inventory.shift()
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+
   }
 }
 
