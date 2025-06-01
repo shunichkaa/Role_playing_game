ee2c8d12dbe5cfb0161aea6590d70f5244153be8
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-01 11:38:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -113,6 +113,9 @@ function sellWeapon() {
     text.innerText = "You sold a " + currentWeapon + ".";
     text.innerText += " In your inventory you have: " + inventory;
   }
+  else {
+    text.innerText = "Don't sell your only weapon!";
+  }
 }
 
 function fightSlime() {
