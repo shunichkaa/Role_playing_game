ce0656b7355d25646d9c5512c8d47003ea99eb0f
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-03 16:20:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -161,6 +161,7 @@ function goFight() {
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
 }
 
 function dodge() {
