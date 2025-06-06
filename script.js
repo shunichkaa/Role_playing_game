2c5105e79b59b7b9a9bc8c1e46d7b4255fcd63a9
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-06 09:35:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -63,14 +63,14 @@ const locations = [
     "button text": ["Attack", "Dodge", "Run"],
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster."
+  },
+  {
+    name: "kill monster",
+    "button text": ["Go to town square", "Go to town square", "Go to town square"],
+    "button functions": [goTown, goTown, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
   }
 ];
-locations.push({
-  name: "kill monster",
-  "button text": ["Go to town square", "Go to town square", "Go to town square"],
-  "button functions": [goTown, goTown, goTown],
-  text: "The monster screams Arg! as it dies. You gain experience points and find gold."
-});
 
 // initialize buttons
 button1.onclick = goStore;
