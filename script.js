2d856feaae95be2d5f73d85cdd14a003981ae594
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-11 10:41:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -266,7 +266,9 @@ function pickEight() {
 
 function pick(guess) {
   const numbers = [];
-while (numbers.length < 10) {
-  numbers.push(Math.floor(Math.random() * 11));
-}
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+text.innerText = "You picked " + guess + ". Here are the random numbers:";
+
 }
