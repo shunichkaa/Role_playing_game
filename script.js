69b73ec4e36fd6cbe07dbf95fd7f8baa0e23a5a8
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-14 12:21:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -269,8 +269,8 @@ function pick(guess) {
   while (numbers.length < 10) {
     numbers.push(Math.floor(Math.random() * 11));
   }
-  text.innerText = "You picked " + guess + ". Here are the random numbers:";
-  text.innerText += " " + numbers.join(", ");
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  text.innerText += numbers.join(", ");
 
   if (numbers.includes(guess)) {
     text.innerText += "\nYou win!";
