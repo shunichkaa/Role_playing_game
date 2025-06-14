942174fb6752b635d6b37065bc633540a493a18b
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-14 12:09:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -199,8 +199,8 @@ function attack() {
     }
   }
   if (Math.random() <= .1) {
-text.innerText += " Your " + inventory.pop() + " breaks.";
-
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeaponIndex--;
   }
 }
 
